e82086914350db7dcb6b774dcb7562b1dc6c8299
migue <114624729+Miguecaritatriste@users.noreply.github.com>
2024-09-08 01:36:15 +0200

    error si no hay user manejado

--- src/pages/Dieta.jsx
+++ src/pages/Dieta.jsx
@@ -18,14 +18,18 @@ function Dieta() {
   const [ObjProteinas, setObjProteinas] = useState(100);
   const [ObjCalorias, setObjCalorias] = useState(2100);
 
+
   axios.get("https://serverc-4y5e.onrender.com/getSession", {
     withCredentials: true,
   }).then((res) => {
+    if (res.data.usuario == null || res.data.usuario == undefined) {
+      return;
+    }
     setUsuario(res.data.usuario);
-    if (res.data.usuario.ObjProteinas) {
+    if (res.data.usuario.ObjProteinas != undefined && res.data.usuario.ObjProteinas != null) {
       setObjProteinas(res.data.usuario.ObjProteinas);
     }
-    if (res.data.usuario.ObjCalorias) {
+    if (res.data.usuario.ObjProteinas != undefined && res.data.usuario.ObjProteinas != null) {
       setObjCalorias(res.data.usuario.ObjCalorias);
     }
   }).catch((error) => {
